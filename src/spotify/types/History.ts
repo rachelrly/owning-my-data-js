f9248015958e619bc07f10feb8ff3b8b2fb90380
Rachel <rachelreillydev@gmail.com>
2022-01-30 12:59:03 -0600

    only stores time props in plays array

--- src/spotify/types/History.ts
+++ src/spotify/types/History.ts
@@ -1,8 +1,5 @@
-import {CountedSongType, PlaysSongType, SongType} from './Song'
+import { CountedSongType, SongType } from './Song'
 
-// Not currently used
 export type HistoryMapType = Map<string, SongType>
 
 export type ListenHistoryMapType = Map<string, CountedSongType>
-
-export type PlaysHistoryMapType = Map<string, PlaysSongType>
